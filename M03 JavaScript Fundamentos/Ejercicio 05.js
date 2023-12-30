fcbb8a6fea7edf569c4e6fa6d26f136ac04a6e67
Gastón Ariel Perrone <gastonarielp1812@gmail.com>
2023-12-30 18:55:58 -0300

    Exercise 5 finished

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -69,6 +69,17 @@ function esVocal(letra) {
    // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
    // Si no es vocal, tambien debe retornar "Dato incorrecto".
    // Tu código:
+   if (letra.length > 1) {
+      return "Dato incorrecto";
+  }
+
+  letra = letra.toLowerCase();
+
+  if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
+      return "Es vocal";
+  } else {
+      return "Dato incorrecto";
+  }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
